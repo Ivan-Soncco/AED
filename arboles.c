4956d0fdd8f9d247f3dd5dec64c74bbfaf9561f0
Ivan-Soncco <isoncco@unsa.edu.pe>
2024-01-03 15:17:33 -0500

    problema de lab hasPathSum

--- arboles.c
+++ arboles.c
@@ -1,5 +1,29 @@
-#include <iostream.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
+struct node{
+	int data;
+	struct node* left;
+	struct node* right;	
+};
+
+struct node* newNode(int data){
+	struct node* node = malloc(sizeof(struct node));
+	node->data = data;
+	node->left = NULL;
+	node->right = NULL;
+}
+
+struct node* insert(struct node* node, int data){
+	if(node == NULL)
+		return newNode(data);
+	if(data <= node->data)
+		node->left = insert(node->left, data);
+	else 
+		node->right = insert(node->right, data);
+	return node;
+}
 
 int size(struct node* node){	
 	if (node == NULL)
@@ -9,13 +33,13 @@ int size(struct node* node){
 }
 
 //PUEDE QUE ESTE MAL, buscar la hoja mas larga, max entre sub iz o sub der
-int maxDepth(struct node* node){
+/*int maxDepth(struct node* node){
 	if (node == NULL)
 		return 0;
 	else
-		return 1+max(maxDepth(root->left),
-				maxDepth(root->right));
-}
+		return 1+max(maxDepth(node->left),
+				maxDepth(node->right));
+}*/
 
 int minValue(struct node* node){
 	assert(node == NULL);
@@ -29,7 +53,7 @@ void printTree(struct node* node){
 		printf("\n");
 	else{
 		printTree(node->left);
-		print("%d ",node->data);
+		printf("%d ",node->data);
 		printTree(node->right);
 
 	}
@@ -37,20 +61,41 @@ void printTree(struct node* node){
 }
 
 void printPostOrder(struct node* node){
-	if(node == NULL)
+	if(node == NULL){return;}
 }
-
+//Tiene que mostrar verdadero si el valor de sum es igual a la suma de cualquiera de los caminos hacia las ramas
 int hasPathSum(struct node* node, int sum){
-	int count= 0;
+	/*//cbase
 	if(node == NULL)
 		return 0;
-	hasPathSum(node->left, sum-(node->value));
-	if(node->left == NULL && node->right == NULL)
-		
 	
+	if(node->left == NULL && node->right == NULL && sum-(node->data)==0)
+		return 1;
+	else{
+	hasPathSum(node->left, sum-(node->data));
+	hasPathSum(node->right, sum - (node->data));
+	}*/
+	if(node == NULL)
+		return(sum == 0);
+	else{
+		int subSum = sum - node->data;
+		return (hasPathSum(node->left, subSum) || hasPathSum(node->right, subSum));
+	}
 }
+// busqueda InOrder: itera sobre el arbol de forma recursiva, para ir a sub--arboles izquierdos al padre y a subarboles derechos para cada subarbol.
+int main(){
+	struct node* node= newNode(5);
+	insert(node,4);
+	insert(node,6);
+	insert(node,8);
+	insert(node,7);
+	//insert(node, 9);
+	struct node* root =NULL;
+	printTree(node);
+	printf("%d \n",hasPathSum(node,26));
+	return 0;
 
-
+}
 
 
 
